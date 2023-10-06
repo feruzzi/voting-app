2cc81c4c20753fb439f6128b1c8330b813d43e9c
Feruzzi Graha <feruzzig@gmail.com>
2023-10-06 20:06:03 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -2,18 +2,44 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Flow</h1>
+            <ul className="steps">
+              <li className="step step-primary">Register</li>
+              <li className="step step-primary">Vote</li>
+              <li className="step">Wait</li>
+              <li className="step">See Result</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">About US</h1>
+            <span className="loading loading-infinity loading-xs"></span>
+            <span className="loading loading-infinity loading-sm"></span>
+            <span className="loading loading-infinity loading-md"></span>
+            <span className="loading loading-infinity loading-lg"></span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
